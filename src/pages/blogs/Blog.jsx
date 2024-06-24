5a236114573cde3f4a0bc7355dd01e280535b0fd
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2024-06-24 06:40:11 +0000

    Worked on Image Adjustment

---
+++ src/pages/blogs/Blog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import MainBody from "./Main";
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <MainBody />
+        </div>
+    )
+}
+export default Main
